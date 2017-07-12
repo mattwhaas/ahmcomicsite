1462d1c95c632b19de525595eedcdb54974d1463
Matthew Haas <matthewhaas@Matthews-MacBook-Pro.local>
2017-07-12 19:40:51 -0400

    added header, copyright, broken menu bar, broken slideshow

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+<script>
+    var imgs=["img/testCmx/testComic1.gif", "img/testCmx/testComic2.gif", "img/testCmx/testComic3.gif"];
+
+    function changeImage(dir) {
+        var img = document.getElementById("imgClickAndChange");
+        img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
+    }
+
+    document.onkeydown = function(e) {
+        e = e || window.event;
+        if (e.keyCode == '37') {
+            changeImage(-1) //left <- show Prev image
+        } else if (e.keyCode == '39') {
+            // right -> show next image
+            changeImage()
+        }
+    }
+</script>    
